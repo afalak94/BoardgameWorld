bf8606cfc3934c265b6fb2cc7554bab8d8b0ac02
afalak94 <antonio.falak@gmail.com>
2018-12-08 20:55:36 +0100

    cart

--- src/modules/Cart/index.js
+++ src/modules/Cart/index.js
@@ -1,12 +1,53 @@
 //Cart component
 import React, { Component } from 'react';
+import {
+  ListGroup,
+  ListGroupItem,
+  ListGroupItemHeading,
+  ListGroupItemText
+} from 'reactstrap';
+import { connect } from 'react-redux';
 
 class Cart extends Component {
-    render() {
-        return (
-            <div className="cart"><h2>Cart screen</h2></div>
-        );
-    }
+  constructor(props) {
+    super(props);
+
+    console.log(this.props.cart);
+  }
+  render() {
+    return (
+      <ListGroup className='cart__listgroup'>
+        {this.props.cart.map((item, i) => {
+          return (
+            <ListGroupItem key={i} className='cart__list'>
+              <div style={{ float: 'left' }}>
+                <img
+                  className='img-thumbnail cart__itemImg'
+                  src={item.imgUrl}
+                />
+              </div>
+
+              <div>
+                <ListGroupItemHeading>{item.name}</ListGroupItemHeading>
+                <ListGroupItemText>
+                  <p>{item.price} $</p>
+                </ListGroupItemText>
+              </div>
+            </ListGroupItem>
+          );
+        })}
+      </ListGroup>
+    );
+  }
 }
 
-export default Cart;
+function mapStateToProps(state) {
+  return {
+    cart: state.cart
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(Cart);
